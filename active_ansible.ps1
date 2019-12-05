465ff05c074e0eb77bceee018c8bbe63c8e5503d
michaellesard <mlessard@redhat.com>
2019-12-05 10:10:10 -0500

    fix scripts

--- active_ansible.ps1
+++ active_ansible.ps1
@@ -1,9 +1,6 @@
-Set-NetConnectionProfile -Name "Network" -NetworkCategory Private
 $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
 powershell.exe -ExecutionPolicy ByPass -File $file
-winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-winrm quickconfig -force
